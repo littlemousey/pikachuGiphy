245afd0f9996258d85e7e820756a09b20a35e9a8
Ans de Nijs <ansdenijs@gmail.com>
2018-02-25 12:48:30 +0100

    Pikachu giphy generator
    First commit :D

---
+++ giphy.js
@@ -0,0 +1,28 @@
+var response;
+var xhr = new XMLHttpRequest();
+// retrieve 200 pikachu url images from Giphy 
+xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=pHeytEtBN04WZZzVbodZiBtWuRodg9tM&q=pikachu&limit=200&offset=0&rating=G&lang=en', true);
+
+xhr.onload = function () {
+  if (xhr.readyState === xhr.DONE) {
+    if (xhr.status === 200) {
+    	response = JSON.parse(xhr.response);
+      	returnPikachuImg(response);
+    }
+  }
+};
+
+xhr.send(null);
+
+
+function returnPikachuImg (response) {
+	let pikachuCounter = randomNumber(0, 200); // to get a random number each time the button is pressed
+	const image = response.data[pikachuCounter].images.original.url;
+	document.getElementById("img").setAttribute("src", image);
+}
+
+function randomNumber (min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
