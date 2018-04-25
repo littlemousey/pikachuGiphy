8c82ad1812d0c38cf04dd005c746c42d8236622d
Ans de Nijs <ansdenijs@gmail.com>
2018-04-25 14:34:40 +0200

    Images loop can be paused + code refactor

--- giphy.js
+++ giphy.js
@@ -1,56 +1,75 @@
-let response;
-let xhr = new XMLHttpRequest();
-// retrieve 500 pikachu url images from Giphy 
-xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=pHeytEtBN04WZZzVbodZiBtWuRodg9tM&q=pikachu&limit=500&offset=0&rating=G&lang=en', true);
+let giphies;
+let timer;
 
-xhr.onload = function () {
-  if (xhr.readyState === xhr.DONE) {
-    if (xhr.status === 200) {
-    	response = JSON.parse(xhr.response);
-    	returnPikachuImg(response);
-      	pikachuTimer(response);
-      	changeButtonProperties();
-    }
-  }
-};
+loadGiphies({
+	apiKey: 'pHeytEtBN04WZZzVbodZiBtWuRodg9tM',
+	subject: 'pikachu'
+});
 
-xhr.send(null);
+function loadGiphies(info) {
+	const xhr = new XMLHttpRequest();
 
+	// retrieve 500 pikachu url images from Giphy 
+	xhr.open('GET', `https://api.giphy.com/v1/gifs/search?api_key=${info.apiKey}&q=${info.subject}&limit=500&offset=0&rating=G&lang=en`);
 
-function returnPikachuImg (response) {
+	xhr.onload = function () {
+	  if (xhr.readyState === xhr.DONE) {
+	    if (xhr.status === 200) {
+	    	const response = JSON.parse(xhr.response);
+
+	    	giphies = extractGiphyUrls(response);
+	      	initializeButtons();
+
+	    	showRandomPikachuImage();
+	      	timer = startPikachuTimer();
+	    }
+	  }
+	};
+
+	xhr.send(null);
+}
+
+function extractGiphyUrls(response) {
+	return response.data.map(function(item) { return item.images.fixed_height.url; });
+}
+
+function initializeButtons () {
+	const nextButton = document.getElementById("next_btn");
+	const playButton = document.getElementById("play_btn");
+	nextButton.innerText = 'Next pikachu';
+
+	nextButton.addEventListener('click', function() {
+		showRandomPikachuImage();
+	});
+	playButton.addEventListener('click', function() {
+		iconAction();
+	});
+}
+
+function showRandomPikachuImage () {
 	let pikachuCounter = randomNumber(0, 500); // to get a random number each time the button is pressed
-	const image = response.data[pikachuCounter].images.fixed_height.url;
+	const image = giphies[pikachuCounter];
 	document.getElementById("img").setAttribute("src", image);
 }
 
-function pikachuTimer(response) {
-	setInterval(function(){ returnPikachuImg(response); }, 5000);
-}
-
-function changeButtonProperties () {
-	const nextButton = document.getElementById("next_btn");
-	const playButton = document.getElementById("play_btn");
-	nextButton.setAttribute('onClick', 'returnPikachuImg(response)');
-	nextButton.innerText = 'Next pikachu';
-	playButton.setAttribute('onClick', 'iconAction()');
+function startPikachuTimer () {
+	return setInterval(function() { showRandomPikachuImage(); }, 5000);
 }
 
 function iconAction () {
+	// to change the play or stop icon and trigger action
 	const icon = document.getElementById("icon");
 	if (playModeOn()) {
+		timer = startPikachuTimer();
 		icon.setAttribute('class', 'fa fa-stop');
 	} else {
+		timer = clearInterval(timer);
 		icon.setAttribute('class', 'fa fa-play');
 	}
 }
 
 function playModeOn () {
-	const icon = document.getElementById("icon");
-	if (icon.className === 'fa fa-play'){
-		return true;
-	} else if (icon.className === 'fa fa-stop') {
-		return false;
-	}
+	return timer !== undefined;
 }
 
 function randomNumber (min, max) {
