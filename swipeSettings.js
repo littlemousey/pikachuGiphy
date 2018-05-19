5ffd41d6e1ea586c29d8bbd88ac54084b2b8bcbd
Ans de Nijs <ansdenijs@gmail.com>
2018-05-19 11:41:29 +0200

    Swipe fix

--- swipeSettings.js
+++ swipeSettings.js
@@ -7,14 +7,12 @@ hammertime.on("swipeleft swiperight", function(ev) {
 	console.log(ev.type +" gesture detected.");
 	console.log(history);
 	if (ev.type === 'swipeleft') {
-		if (timer !== undefined) {
-			timer = startPikachuTimer();
-		} else {
-			showRandomPikachuImage();
-		}
+		showRandomPikachuImage();
+
 	} else if (ev.type === 'swiperight') {
 		timer = clearInterval(timer);
 		document.getElementById("img").setAttribute("src", history[history.length-1]);
 		history.pop();
+		timer = startPikachuTimer();
 	}
 });
