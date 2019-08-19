df29a4b6cef0bb8e53397ff0260cbb75a4edb6dc
Ans de Nijs <ansdenijs@gmail.com>
2019-08-19 10:07:57 +0200

    Unused code removed

--- swipeSettings.js
+++ swipeSettings.js
@@ -1,20 +1,19 @@
-const element = document.querySelector('body');
+const element = document.querySelector("body");
 let hammertime = new Hammer(element);
 
-hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+hammertime.get("pan").set({ direction: Hammer.DIRECTION_ALL });
 
 hammertime.on("swipeleft swiperight", function(ev) {
-	if (ev.type === 'swipeleft') {
-		showRandomPikachuImage();
-
-	} else if (ev.type === 'swiperight') {
-		if (history.length > 0) {
-			timer = clearInterval(timer);
-			console.log(history);
-			document.getElementById("img").setAttribute("src", history[history.length-1]);
-			console.log(history);
-			history.pop();
-			timer = startPikachuTimer();
-		}
-	}
+  if (ev.type === "swipeleft") {
+    showRandomPikachuImage();
+  } else if (ev.type === "swiperight") {
+    if (history.length > 0) {
+      timer = clearInterval(timer);
+      document
+        .getElementById("img")
+        .setAttribute("src", history[history.length - 1]);
+      history.pop();
+      timer = startPikachuTimer();
+    }
+  }
 });
